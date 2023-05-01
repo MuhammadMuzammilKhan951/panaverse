f0bede01c5a672edd16f6ff5fb9b23753446e80f
MuhammadMuzammilKhan951 <115939283+MuhammadMuzammilKhan951@users.noreply.github.com>
2023-05-01 07:50:23 -0700

    Update dropdown.tsx

--- src/app/dropdown.tsx
+++ src/app/dropdown.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function dropdown() {
   return (
-    <div className='  m:hidden l:hidden ll:hidden lll:hidden'>
+    <div className='mt-[-13px]  m:hidden l:hidden ll:hidden lll:hidden'>
       <div className="dropdown dropdown-end">
   <label tabIndex={0} className="btn m-1 bg-slate-900 rounded-full pr-2 pl-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className=" h-6 sw-6">
   <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
